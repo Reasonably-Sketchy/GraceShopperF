5d052876b25fab38dfc44529756335b9f7f654a8
Nick Swanson <nickjswanson@yahoo.com>
2021-04-10 12:38:16 -0700

    getting a homepage

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,14 @@
 // For exporting page components
-export {default as Header} from './header/Header';
+export { default as Header } from "./header/Header";
 
-export {default as Welcome} from './login/Welcome';
-export {default as Login} from './login/Login';
-export {default as Register} from './login/Register';
+export { default as Welcome } from "./login/Welcome";
+export { default as Login } from "./login/Login";
+export { default as Register } from "./login/Register";
 
-export {default as Cart} from 'cart/Cart';
+export { default as Cart } from "./cart/Cart";
 
-export {default as Products} from './products/Products';
-export {default as SingleProduct} from './products/SingleProduct';
+export { default as Products } from "./products/Products";
+export { default as SingleProduct } from "./products/SingleProduct";
 
-export {default as Account} from './account/Account';
-export {default as SingleOrder} from './orders/SingleOrder';
+export { default as Account } from "./account/Account";
+export { default as SingleOrder } from "./orders/SingleOrder";
--- src/index.js
+++ src/index.js
@@ -1,135 +1,129 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    useHistory,
-    Link
-} from 'react-router-dom';
-import { fetchUserData, fetchAllProducts } from './api/utils';
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+  Link,
+} from "react-router-dom";
+import { fetchUserData, fetchAllProducts } from "./api/utils";
 
 // Page components
-import { 
-    Header,
-    Welcome,
-    Login,
-    Register,
-    Products,
-    SingleProduct,
-    Account,
-    SingleOrder, 
-} from './components'
+import {
+  Header,
+  Welcome,
+  Login,
+  Register,
+  Products,
+  SingleProduct,
+  Account,
+  SingleOrder,
+  Cart,
+} from "./components";
 
-import Cart from './components/cart/Cart.js'
+// import Cart from './components/cart/Cart.js'
 
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import './styles.css';
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import "./styles.css";
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            // main: '#d4af37',
-            main: '#b5a264',
-        },
-        secondary: {
-            main: '#222',
-        },
-    }
+  palette: {
+    primary: {
+      // main: '#d4af37',
+      main: "#b5a264",
+    },
+    secondary: {
+      main: "#222",
+    },
+  },
 });
 
 const App = () => {
-    const [token, setToken] = useState("");
-    const [userData, setUserData] = useState({});
-    const [allProducts, setAllProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+  const [token, setToken] = useState("");
+  const [userData, setUserData] = useState({});
+  const [allProducts, setAllProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    // Retrieve token from local storage
-    useEffect(async () => {
-        if (!token) {
-            setToken(localStorage.getItem('token'));
-            return;
-        };
+  // Retrieve token from local storage
+  useEffect(async () => {
+    if (!token) {
+      setToken(localStorage.getItem("token"));
+      return;
+    }
 
-        const data = await fetchUserData(token);
-        if (data && data.username) {
-            setUserData(data);
-        };
-    }, [token])
+    const data = await fetchUserData(token);
+    if (data && data.username) {
+      setUserData(data);
+    }
+  }, [token]);
 
-    // Retrieve all products
-    useEffect(async () => {
-        try {
-            const products = await fetchAllProducts(token);
-            console.log('Products: ', products);
-            if (products) {
-                setAllProducts(products);
-            };
+  // Retrieve all products
+  useEffect(async () => {
+    try {
+      const products = await fetchAllProducts(token);
+      console.log("Products: ", products);
+      if (products) {
+        setAllProducts(products);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
-        } catch(error) {
-            console.error(error);
-        };
-    }, [])
+  return (
+    <div id="app">
+      <ThemeProvider theme={theme}>
+        <Header />
+        {/* <h1>Grace Shopper</h1> */}
 
-    return (
-        <div id="app">
-            <ThemeProvider theme={theme}>
-                <Header />
-                {/* <h1>Grace Shopper</h1> */}
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
 
-                <Switch>
+          <Route path="/login">
+            <Login setToken={setToken} />
+          </Route>
 
-                    <Route path = "/welcome">
-                        <Welcome />
-                    </Route>
+          <Route path="/register">
+            <Register setToken={setToken} />
+          </Route>
 
-                    <Route path = "/login">
-                        <Login 
-                            setToken = {setToken} />
-                    </Route>
+          <Route exact path="/products">
+            <Products allProducts={allProducts} />
+          </Route>
 
-                    <Route path ="/register">
-                        <Register
-                            setToken = {setToken} />
-                    </Route>
+          <Route exact path="/products/:productId">
+            <SingleProduct allProducts={allProducts} />
+          </Route>
 
-                    <Route exact path = "/products">
-                        <Products allProducts = {allProducts}/>
-                    </Route>
+          <Route path="/account">
+            <Account userData={userData} />
+          </Route>
 
-                    <Route exact path = "/products/:productId">
-                        <SingleProduct allProducts = {allProducts}/>
-                    </Route>
+          {userData.isAdmin ? (
+            <Route path="/orders/:orderId">
+              <SingleOrder />
+            </Route>
+          ) : (
+            ""
+          )}
 
-                    <Route path = "/account">
-                        <Account 
-                            userData = {userData} />
-                    </Route>
-
-                    {userData.isAdmin
-                    ? <Route path = "/orders/:orderId">
-                        <SingleOrder />
-                    </Route>
-                    : ''}
-
-                    <Route path = "/cart">
-                        <Cart cart={cart} setCart={setCart}/>
-                    </Route>
-
-
-                </Switch>
-
-                
-            </ThemeProvider>
-        </div>
-    );
+          <Route path="/cart">
+            <Cart cart={cart} setCart={setCart} />
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default App;
 
-
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, 
-    document.getElementById('root'));
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
