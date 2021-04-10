39e0a0d06e49ff9869708d1bfd25480029379dc6
Nick Swanson <nickjswanson@yahoo.com>
2021-04-10 10:10:09 -0700

    stuff that will be overwritten

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,65 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import LogoButton from './LogoButton';
-import NavList from './NavList';
-import Burger from './Burger';
-import './Header.css';
+import LogoButton from "./LogoButton";
+import NavList from "./NavList";
+import Burger from "./Burger";
+import "./Header.css";
 
 const Header = ({ setToken, setUserData, userData }) => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-   
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [activeLinkIs, setActiveLinkIs] = useState('Home');
-    const [clientWidth, setClientWidth] = useState(0);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    const changeActiveLink = (url, setActiveLinkIs) => {
-        // if (url === 'http://localhost:3000/' || url === 'https://our-url.com/')
-        if (url === 'http://localhost:3001/') {
-            setActiveLinkIs("Home")
-        };
-        
-        if (url.includes('/products')) {
-            setActiveLinkIs("Products")
-        };
-    
-        if (url.includes('/account')) {
-            setActiveLinkIs("Account")
-        };
-    
-        if (url.includes('/admin')) {
-            setActiveLinkIs("Admin")
-        };
+  const [activeLinkIs, setActiveLinkIs] = useState("Home");
+  const [clientWidth, setClientWidth] = useState(0);
 
-        if (url.includes('/welcome') || url.includes('/login') || url.includes('/register')) {
-            setActiveLinkIs("Welcome")
-        };
-    };
+  const changeActiveLink = (url, setActiveLinkIs) => {
+    // if (url === 'http://localhost:3000/' || url === 'https://our-url.com/')
+    if (url === "http://localhost:3001/") {
+      setActiveLinkIs("Home");
+    }
 
-    useEffect(() => {
-        setClientWidth(window.innerWidth);
-        changeActiveLink(window.location.href, setActiveLinkIs);
-    });
+    if (url.includes("/products")) {
+      setActiveLinkIs("Products");
+    }
 
-    return (
-        <header id="header">
-            <LogoButton />
-            <NavList 
-                // setToken={setToken}
-                // setUserData={setUserData}
-                isMenuOpen = {isMenuOpen} 
-                toggleMenu = {toggleMenu}
-                activeLinkIs = {activeLinkIs}
-                // userData = {userData} 
-                />
-            
-            <Burger 
-                isMenuOpen = {isMenuOpen} 
-                toggleMenu = {toggleMenu} />
-        </header>
-    );
+    if (url.includes("/account")) {
+      setActiveLinkIs("Account");
+    }
+
+    if (url.includes("/admin")) {
+      setActiveLinkIs("Admin");
+    }
+
+    if (
+      url.includes("/welcome") ||
+      url.includes("/login") ||
+      url.includes("/register")
+    ) {
+      setActiveLinkIs("Welcome");
+    }
+
+    if (url.includes("cart")) {
+      setActiveLinkIs("Cart");
+    }
+  };
+
+  useEffect(() => {
+    setClientWidth(window.innerWidth);
+    changeActiveLink(window.location.href, setActiveLinkIs);
+  });
+
+  return (
+    <header id="header">
+      <LogoButton />
+      <NavList
+        // setToken={setToken}
+        // setUserData={setUserData}
+        isMenuOpen={isMenuOpen}
+        toggleMenu={toggleMenu}
+        activeLinkIs={activeLinkIs}
+        // userData = {userData}
+      />
+
+      <Burger isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
+    </header>
+  );
 };
 
 export default Header;
