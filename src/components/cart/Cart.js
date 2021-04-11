0a166aaf5bd5eb03781c4ca8ac852db6c7fa78a4
Nick Swanson <nickjswanson@yahoo.com>
2021-04-10 17:50:06 -0700

    chore: minor changes to display cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import './Cart.css';
+import React from "react";
+import "./Cart.css";
+// import Orders from "../orders/Orders.js";
 
-const Cart = ({cart, setCart})=>{
-
-    return(
-        <Orders orders={cart} setOrders={setCart}/>
-    );
-}
+const Cart = ({ cart, setCart }) => {
+  //   return <Orders orders={cart} setOrders={setCart} />;
+  return <div>Cart Page Sucess!</div>;
+};
 
 export default Cart;
