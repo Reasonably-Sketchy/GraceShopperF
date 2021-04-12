d684fe11715c38e7149da33d3aeea21ade92267e
Austin <austin.thomas130@gmail.com>
2021-04-12 10:02:52 -0700

    fix: reworked single product page styling and began implementing quantity control and add to cart button

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -4,10 +4,12 @@ import { useParams } from 'react-router';
 
 import ProductCard from './ProductCard';
 
-import './Products.css';
+// import './Products.css';
+import './SingleProduct.css';
 
 const SingleProduct = ({ allProducts }) => {
     const { productId } = useParams();
+    const [quantity, setQuantity] = useState(1);
 
     if (!allProducts) {
         return <h1>Loading...</h1>
@@ -19,15 +21,39 @@ const SingleProduct = ({ allProducts }) => {
         return <h1>This product does not exist.</h1>
     };
     
+    const handleQuantityChange = async (event) => {
+        setQuantity(Number(event.target.value))
+    };
+
     return (
-        <main id="products">
-            <h1>{thisProduct.name}</h1>
-            <img className="product-picture" src={thisProduct.imageURL} />
-            <h2>{thisProduct.price} USD</h2>
-            <p>{thisProduct.description}</p>
-            <Button
-                variant="outlined"
-                color="primary">Add to Cart</Button>
+        <main id="single-product">
+            <section className="single-product-image-container">
+                <img className="product-picture" src={thisProduct.imageURL} />
+            </section>
+
+            <section className="single-product-description">
+                <h1>{thisProduct.name}</h1>
+                <h3 className="description">{thisProduct.description}</h3>
+                <h3><span className="gold-text">{thisProduct.price}</span> USD</h3>
+                
+                <div className="action-container">
+
+                    <fieldset className="quantity-container">
+                        <label htmlFor="quantity">Quantity:</label>
+                        <input 
+                            name="quantity"
+                            type="number"
+                            value={quantity}
+                            onChange={handleQuantityChange}></input>
+                    </fieldset>
+
+                    <Button
+                        variant="outlined"
+                        color="primary">Add to Cart</Button>
+                </div>
+
+            </section>
+
         </main>
     );
 };
