2ebe8698435b94d0d2f87ba33650d509397d4de8
Nick Swanson <nickjswanson@yahoo.com>
2021-04-19 19:52:25 -0700

    new and improved loading apparatus

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -1,244 +1,314 @@
-import React, {useEffect, useState} from 'react';
-import { useHistory, useParams } from 'react-router';
-import { addProductToOrder, createOrder, fetchReviews, fetchUserCart, updateUserData } from '../../api/utils';
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router";
+import {
+  addProductToOrder,
+  createOrder,
+  fetchReviews,
+  fetchUserCart,
+  updateUserData,
+} from "../../api/utils";
 
-import { Button } from '@material-ui/core';
-import { KeyboardArrowLeft, KeyboardArrowDown, KeyboardArrowRight } from '@material-ui/icons';
+import { Button } from "@material-ui/core";
+import {
+  KeyboardArrowLeft,
+  KeyboardArrowDown,
+  KeyboardArrowRight,
+} from "@material-ui/icons";
 
-import ReviewCard from '../reviews/ReviewCard';
-import ReviewCreator from '../reviews/ReviewCreator';
-import EditProduct from './EditProduct';
-import AdminModal from '../admin/AdminModal';
+import ReviewCard from "../reviews/ReviewCard";
+import ReviewCreator from "../reviews/ReviewCreator";
+import EditProduct from "./EditProduct";
+import AdminModal from "../admin/AdminModal";
 
+import "./SingleProduct.css";
 
-import './SingleProduct.css';
+const SingleProduct = ({
+  allProducts,
+  cart,
+  setCart,
+  token,
+  setUserData,
+  userData,
+  setAllProducts,
+}) => {
+  const history = useHistory();
+  const { productId } = useParams();
+  const [quantity, setQuantity] = useState(1);
+  const [respMessage, setRespMessage] = useState("");
+  const [editExpand, setEditExpand] = useState(false);
+  const [reviews, setReviews] = useState([]);
+  const [creatorOpen, setCreatorOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [numLoadingEvents, setNumLoadingEvents] = useState(0);
 
-const SingleProduct = ({ allProducts, cart, setCart, token, setUserData, userData, setAllProducts }) => {
-    const history = useHistory();
-    const { productId } = useParams();
-    const [quantity, setQuantity] = useState(1);
-    const [respMessage, setRespMessage] = useState('');
-    const [editExpand, setEditExpand] = useState(false);
-    const [reviews, setReviews] = useState([]);
-    const [creatorOpen, setCreatorOpen] = useState(false);
-    const [modalOpen, setModalOpen] = useState(false);
-
-    useEffect(async () => {
-        const productReviews = await fetchReviews(productId);
-        if (productReviews) {
-            setReviews(productReviews);
-        };
-    }, []);
-
-    if (!allProducts) {
-        return <h3>Loading...</h3>
-    };
-
-    const thisProduct = allProducts.find((product) => {return product.id == productId})
-
-    if (!thisProduct) {
-        return <h3>This product does not exist.</h3>
-    };
-
-    const handleQuantityChange = async (event) => {
-        setQuantity(Number(event.target.value))
-    };
-
-    const modalCloseFunction = () => {
-        setModalOpen(false);
-        history.push('/products');
-    };
-
-
-    const handleAddToCart = async () => {
-        // IF I'M NOT LOGGED IN
-        if (!token) {
-            const toBeOrderProduct = {
-                productId: thisProduct.id,
-                name: thisProduct.name,
-                description: thisProduct.description,
-                imageURL: thisProduct.imageURL,
-                price: thisProduct.price,
-                quantity: quantity,
-            };
-
-            let cartCopy = [];
-            if (cart && cart.length > 0) {
-                cartCopy = [...cart];
-                for (let i = 0; i < cartCopy.length; i ++) {
-                    const checkedProduct = cartCopy[i];
-                    if (checkedProduct.name === thisProduct.name) {
-                        setRespMessage('This item is already in your cart.');
-                        return;
-                    };
-                };
-            };
-
-            cartCopy.push(toBeOrderProduct);
-            setCart(cartCopy);
-            localStorage.setItem('cart', JSON.stringify(cartCopy));
-            setRespMessage('Added to cart!')
-        };
-
-        // IF I AM LOGGED IN
-        if (token) {
-            // try to get my cart
-            let databaseCart = await fetchUserCart(token);
-
-            // If there's nothing in the cart state, either find my empty cart or create a new order
-            if (cart.length === 0) {
-                // Locate the user's cart (or create one if there is none)
-                if (databaseCart && databaseCart.length > 0) {
-                    console.log("Existing User Cart: ", databaseCart);
-                } else if (!databaseCart) {
-                    databaseCart = await createOrder(token);
-                    console.log("New User Cart:", databaseCart);
-                };
-            };
-
-            // Add the product to the order
-            const body = {
-                productId: productId,
-                price: thisProduct.price,
-                quantity: quantity,
-            };
-
-            const newOrderProduct = await addProductToOrder(databaseCart.id, body, token);
-
-            // Need to update the cart
-            let cartCopy = [];
-            if (cart && cart.length > 0) {
-                cartCopy = [...cart];
-                for (let i = 0; i < cartCopy.length; i ++) {
-                    const checkedProduct = cartCopy[i];
-                    if (checkedProduct.name === thisProduct.name) {
-                        setRespMessage('This item is already in your cart.');
-                        return;
-                    };
-                };
-            };
-            cartCopy.push(newOrderProduct);
-            setCart(cartCopy);
-            setRespMessage('Added to cart!')
-            await updateUserData(token, setUserData);
-        };
-
-    };
-
-    return (
-        <main id="single-product">
-
-            <div className="back-to-products">
-                <Button
-                    color="primary"
-                    onClick={() => {
-                        history.push('/products');
-                    }}><KeyboardArrowLeft />All Products</Button>
-            </div>
-
-            <div className="single-product-body">
-                <section className="single-product-image-container">
-                    <img src={thisProduct.imageURL} />
-                </section>
-
-                <section className="single-product-description">
-                    <h1>{thisProduct.name}</h1>
-                    <h3 className="description">{thisProduct.description}</h3>
-                    <h3><span className="gold-text">{thisProduct.price}</span> USD</h3>
-                    
-                    <div className="action-container">
-
-                        <fieldset className="quantity-container">
-                            <label htmlFor="quantity">Quantity:</label>
-                            <input 
-                                className="quantity-input"
-                                name="quantity"
-                                type="number"
-                                value={quantity}
-                                onChange={handleQuantityChange}></input>
-                        </fieldset>
-                        
-                        <div className="action-buttons">
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                onClick={handleAddToCart}>Add to Cart</Button>
-
-                            {userData.isAdmin
-                            ? <Button
-                                variant="contained"
-                                color="secondary"
-                                onClick={() => {
-                                    setEditExpand(!editExpand);
-                                }}>Edit Product {editExpand ? <KeyboardArrowDown /> : <KeyboardArrowRight />}</Button>
-                            : ''}
-                        </div>
-                        
-
-                    </div>
-
-                    <div className="resp-message">
-                        {respMessage ? <div className="respMessage">{respMessage}</div> : ''}
-                    </div>
-
-                </section>
-
-                        {editExpand
-                        ? <center>
-                            <EditProduct 
-                                thisProduct = {thisProduct}
-                                token = {token}
-                                setEditExpand = {setEditExpand}
-                                setModalOpen = {setModalOpen}
-                                setAllProducts = {setAllProducts}/>
-                        </center>
-                        : ''
-                        } 
-
-                <section className="reviews">
-                    <h2>Reviews:</h2>
-                    <Button
-                        className="add-review"
-                        variant="contained"
-                        color="primary"
-                        disabled={userData && userData.username ? false : true}
-                        onClick={() => {
-                            setCreatorOpen(true);
-                        }}>Add a Review</Button>
-                    <div className="reviews-container">
-                        {reviews && reviews.length > 0
-                        ? reviews.map((review) => {
-                            return (
-                                <ReviewCard
-                                    key = {review.id} 
-                                    review = {review}
-                                    userData = {userData}
-                                    token = {token}
-                                    setReviews = {setReviews} />
-                            );
-                        })
-                        : <div>No reviews to display.</div>
-                        }
-                    </div>
-                </section>
-
-                {creatorOpen
-                ? <ReviewCreator 
-                    token = {token}
-                    productId = {productId}
-                    setCreatorOpen = {setCreatorOpen}
-                    setReviews = {setReviews}/>
-                : ''}
-
-                {modalOpen 
-                ? <AdminModal 
-                    action = {"Product Updated"}
-                    data = {thisProduct.name}
-                    modalCloseFunction = {modalCloseFunction}/>
-                : ''}
-            </div>
-        </main>
+  useEffect(async () => {
+    const productReviews = await fetchReviews(
+      productId,
+      numLoadingEvents,
+      setNumLoadingEvents
     );
+    if (productReviews) {
+      setReviews(productReviews);
+    }
+  }, []);
+
+  if (!allProducts) {
+    return <h3>Loading...</h3>;
+  }
+
+  const thisProduct = allProducts.find((product) => {
+    return product.id == productId;
+  });
+
+  if (!thisProduct) {
+    return <h3>This product does not exist.</h3>;
+  }
+
+  const handleQuantityChange = async (event) => {
+    setQuantity(Number(event.target.value));
+  };
+
+  const modalCloseFunction = () => {
+    setModalOpen(false);
+    history.push("/products");
+  };
+
+  const handleAddToCart = async () => {
+    // IF I'M NOT LOGGED IN
+    if (!token) {
+      const toBeOrderProduct = {
+        productId: thisProduct.id,
+        name: thisProduct.name,
+        description: thisProduct.description,
+        imageURL: thisProduct.imageURL,
+        price: thisProduct.price,
+        quantity: quantity,
+      };
+
+      let cartCopy = [];
+      if (cart && cart.length > 0) {
+        cartCopy = [...cart];
+        for (let i = 0; i < cartCopy.length; i++) {
+          const checkedProduct = cartCopy[i];
+          if (checkedProduct.name === thisProduct.name) {
+            setRespMessage("This item is already in your cart.");
+            return;
+          }
+        }
+      }
+
+      cartCopy.push(toBeOrderProduct);
+      setCart(cartCopy);
+      localStorage.setItem("cart", JSON.stringify(cartCopy));
+      setRespMessage("Added to cart!");
+    }
+
+    // IF I AM LOGGED IN
+    if (token) {
+      // try to get my cart
+      let databaseCart = await fetchUserCart(
+        token,
+        numLoadingEvents,
+        setNumLoadingEvents
+      );
+
+      // If there's nothing in the cart state, either find my empty cart or create a new order
+      if (cart.length === 0) {
+        // Locate the user's cart (or create one if there is none)
+        if (databaseCart && databaseCart.length > 0) {
+          console.log("Existing User Cart: ", databaseCart);
+        } else if (!databaseCart) {
+          databaseCart = await createOrder(token);
+          console.log("New User Cart:", databaseCart);
+        }
+      }
+
+      // Add the product to the order
+      const body = {
+        productId: productId,
+        price: thisProduct.price,
+        quantity: quantity,
+      };
+
+      const newOrderProduct = await addProductToOrder(
+        databaseCart.id,
+        body,
+        token,
+        numLoadingEvents,
+        setNumLoadingEvents
+      );
+
+      // Need to update the cart
+      let cartCopy = [];
+      if (cart && cart.length > 0) {
+        cartCopy = [...cart];
+        for (let i = 0; i < cartCopy.length; i++) {
+          const checkedProduct = cartCopy[i];
+          if (checkedProduct.name === thisProduct.name) {
+            setRespMessage("This item is already in your cart.");
+            return;
+          }
+        }
+      }
+      cartCopy.push(newOrderProduct);
+      setCart(cartCopy);
+      setRespMessage("Added to cart!");
+      await updateUserData(
+        token,
+        setUserData,
+        numLoadingEvents,
+        setNumLoadingEvents
+      );
+    }
+  };
+
+  return (
+    <main id="single-product">
+      <div className="back-to-products">
+        <Button
+          color="primary"
+          onClick={() => {
+            history.push("/products");
+          }}
+        >
+          <KeyboardArrowLeft />
+          All Products
+        </Button>
+      </div>
+
+      <div className="single-product-body">
+        <section className="single-product-image-container">
+          <img src={thisProduct.imageURL} />
+        </section>
+
+        <section className="single-product-description">
+          <h1>{thisProduct.name}</h1>
+          <h3 className="description">{thisProduct.description}</h3>
+          <h3>
+            <span className="gold-text">{thisProduct.price}</span> USD
+          </h3>
+
+          <div className="action-container">
+            <fieldset className="quantity-container">
+              <label htmlFor="quantity">Quantity:</label>
+              <input
+                className="quantity-input"
+                name="quantity"
+                type="number"
+                value={quantity}
+                onChange={handleQuantityChange}
+              ></input>
+            </fieldset>
+
+            <div className="action-buttons">
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleAddToCart}
+              >
+                Add to Cart
+              </Button>
+
+              {userData.isAdmin ? (
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => {
+                    setEditExpand(!editExpand);
+                  }}
+                >
+                  Edit Product{" "}
+                  {editExpand ? <KeyboardArrowDown /> : <KeyboardArrowRight />}
+                </Button>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+
+          <div className="resp-message">
+            {respMessage ? (
+              <div className="respMessage">{respMessage}</div>
+            ) : (
+              ""
+            )}
+          </div>
+        </section>
+
+        {editExpand ? (
+          <center>
+            <EditProduct
+              thisProduct={thisProduct}
+              token={token}
+              setEditExpand={setEditExpand}
+              setModalOpen={setModalOpen}
+              setAllProducts={setAllProducts}
+            />
+          </center>
+        ) : (
+          ""
+        )}
+
+        <section className="reviews">
+          <h2>Reviews:</h2>
+          <Button
+            className="add-review"
+            variant="contained"
+            color="primary"
+            disabled={userData && userData.username ? false : true}
+            onClick={() => {
+              setCreatorOpen(true);
+            }}
+          >
+            Add a Review
+          </Button>
+          <div className="reviews-container">
+            {reviews && reviews.length > 0 ? (
+              reviews.map((review) => {
+                return (
+                  <ReviewCard
+                    key={review.id}
+                    review={review}
+                    userData={userData}
+                    token={token}
+                    setReviews={setReviews}
+                  />
+                );
+              })
+            ) : (
+              <div>No reviews to display.</div>
+            )}
+          </div>
+        </section>
+
+        {creatorOpen ? (
+          <ReviewCreator
+            token={token}
+            productId={productId}
+            setCreatorOpen={setCreatorOpen}
+            setReviews={setReviews}
+          />
+        ) : (
+          ""
+        )}
+
+        {modalOpen ? (
+          <AdminModal
+            action={"Product Updated"}
+            data={thisProduct.name}
+            modalCloseFunction={modalCloseFunction}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+      {numLoadingEvents > 0 ? (
+        <div className="loadingMessage">Loading...</div>
+      ) : (
+        <></>
+      )}
+    </main>
+  );
 };
 
 export default SingleProduct;
--- src/components/reviews/DeleteModal.js
+++ src/components/reviews/DeleteModal.js
@@ -1,57 +1,79 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
-import { Star, StarBorder } from '@material-ui/icons';
+import { Button } from "@material-ui/core";
+import React from "react";
+import { Star, StarBorder } from "@material-ui/icons";
 
-import './DeleteModal.css';
-import { deleteReview, fetchReviews } from '../../api/utils';
+import "./DeleteModal.css";
+import { deleteReview, fetchReviews } from "../../api/utils";
 
-const DeleteModal = ({ setDeleteModalOpen, review, token, setReviews, cardStars }) => {
-
-    const handleDelete = async (event) => {
-        event.preventDefault();
-        const deletedReview = await deleteReview(review.id, token);
-        const updateReviews = await fetchReviews(review.productId);
-        setReviews(updateReviews);
-        setDeleteModalOpen(false);
-    };
-
-    return(
-        <div className="editor-background">
-        <div className='delete-modal'>
-            <div className="delete-modal-content">
-                <h2>Delete this review?</h2>
-                
-                <div className="review">
-                    <h2 className="gold-text">"{review.title}"</h2>
-                    <div className="stars-container">
-                        {cardStars.map((value) => {
-                            if (value) {
-                                return <Star />
-                            } else {
-                                return <StarBorder />
-                            };
-                        })}
-                    </div>
-                    <p>{review.content}</p>
-
-                </div>
-
-                <div className="actions">
-                    <Button 
-                        variant="outlined"
-                        color="primary"
-                        onClick={() => {
-                            setDeleteModalOpen(false);
-                        }}>Cancel</Button>
-                    <Button 
-                        variant="contained"
-                        color="primary"
-                        onClick={handleDelete}>Delete Review</Button>
-                </div>
-            </div>
-        </div>
-        </div>
+const DeleteModal = ({
+  setDeleteModalOpen,
+  review,
+  token,
+  setReviews,
+  cardStars,
+}) => {
+  const [numLoadingEvents, setNumLoadingEvents] = useState(0);
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    const [numLoadingEvents, setNumLoadingEvents] = useState(0);
+    const deletedReview = await deleteReview(
+      review.id,
+      token,
+      numLoadingEvents,
+      setNumLoadingEvents
     );
+    const updateReviews = await fetchReviews(
+      review.productId,
+      numLoadingEvents,
+      setNumLoadingEvents
+    );
+    setReviews(updateReviews);
+    setDeleteModalOpen(false);
+  };
+
+  return (
+    <div className="editor-background">
+      <div className="delete-modal">
+        <div className="delete-modal-content">
+          <h2>Delete this review?</h2>
+
+          <div className="review">
+            <h2 className="gold-text">"{review.title}"</h2>
+            <div className="stars-container">
+              {cardStars.map((value) => {
+                if (value) {
+                  return <Star />;
+                } else {
+                  return <StarBorder />;
+                }
+              })}
+            </div>
+            <p>{review.content}</p>
+          </div>
+
+          <div className="actions">
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => {
+                setDeleteModalOpen(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button variant="contained" color="primary" onClick={handleDelete}>
+              Delete Review
+            </Button>
+          </div>
+        </div>
+      </div>
+      {numLoadingEvents > 0 ? (
+        <div className="loadingMessage">Loading...</div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default DeleteModal;
